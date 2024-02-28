f5164cce790d71e69416dd7bfb943574834e7fd2
jun-oishi <oishi.junnosuke@gmail.com>
2024-02-28 18:46:44 +0900

    minimum cpp rmc

---
+++ src/SpectraSpark/rmc/main.cpp
@@ -0,0 +1,73 @@
+
+#include <chrono>
+#include <iostream>
+#include <string>
+
+#include "rmc.cpp"
+
+using namespace std;
+
+int main() {
+  chrono::system_clock::time_point start, now;
+  start = chrono::system_clock::now();
+  Simulator sim;
+  string src, dst;
+  int Lx, Ly, n, max_iter, move_per_step;
+  double res_thresh, sigma2;
+  double q_min = 4.0, q_max = 7.0;
+
+  cout << "Enter the source file name : ";
+  cin >> src;
+  sim.load_exp_data(src);
+  sim.set_q_range(q_min, q_max);
+  now = chrono::system_clock::now();
+  cout << "Data loaded in "
+       << chrono::duration_cast<chrono::milliseconds>(now - start).count()
+       << "ms" << endl;
+
+  cout << "Enter the destination file name : ";
+  cin >> dst;
+
+  cout << "Lx : ";
+  cin >> Lx;
+  cout << "Ly : ";
+  cin >> Ly;
+  cout << "n : ";
+  cin >> n;
+  // Lx = 90;
+  // Ly = 90;
+  // n = 465;
+  sim.set_Lx(Lx);
+  sim.set_Ly(Ly);
+  sim.set_n(n);
+
+  cout << "max_iter : ";
+  cin >> max_iter;
+  cout << "res_thresh : ";
+  cin >> res_thresh;
+  cout << "sigma2 coeff: ";
+  cin >> sigma2;
+
+  sim.init();
+  now = chrono::system_clock::now();
+  cout << "Initialized in "
+       << chrono::duration_cast<chrono::milliseconds>(now - start).count()
+       << "ms" << endl;
+
+  res_thresh *= sim.get_residual();
+  sigma2 = sim.get_residual() * sigma2;
+  move_per_step = 3;
+
+  sim.run(max_iter, res_thresh, sigma2, move_per_step);
+  now = chrono::system_clock::now();
+  cout << max_iter << " steps finished in "
+       << chrono::duration_cast<chrono::milliseconds>(now - start).count()
+       << "ms" << endl;
+
+  sim.save_result(dst);
+  now = chrono::system_clock::now();
+  cout << "Result saved in "
+       << chrono::duration_cast<chrono::milliseconds>(now - start).count()
+       << "ms" << endl;
+  return 0;
+}
---
+++ src/SpectraSpark/rmc/rmc.cpp
@@ -0,0 +1,415 @@
+
+#include "rmc.hpp"
+
+#include <filesystem>
+#include <random>
+#include <sstream>
+#include <string>
+
+using namespace std;
+using namespace Eigen;
+
+#define MOD(x, y) (((x) % (y) + (y)) % (y))
+#define REV(x) (MOD((x) + 3, 6))
+
+#define TOROW(out, vec)                                          \
+  for (int __i_for_TOROW = 0; __i_for_TOROW < vec.size(); __i_for_TOROW++) { \
+    out << vec(__i_for_TOROW) << " ";                                \
+  };
+
+# define SLICE(to, from, ini, fin) \
+  to.resize((fin) - (ini)); \
+  for (int __i_for_SLICE = 0; __i_for_SLICE < (fin) - (ini); __i_for_SLICE++) { \
+    to(__i_for_SLICE) = from(__i_for_SLICE + (ini)); \
+  };
+
+// random_device seed_gen;
+// mt19937 engine(seed_gen());
+int seed = 0;
+mt19937 engine(seed);
+
+/** vectorをランダムにシャッフルする */
+void random_shuffle(vector<int> &v) { shuffle(v.begin(), v.end(), engine); }
+
+/** 0からn-1の整数で埋められたvectorを返す */
+vector<int> range(const int n) {
+  vector<int> v(n);
+  for (int i = 0; i < n; i++) v[i] = i;
+  return v;
+}
+
+void Simulator::load_exp_data(const string &filename) {
+  ifstream fin(filename);
+  if (!fin) {
+    cerr << "Error: cannot open file `" << filesystem::current_path()
+         << filename << "`" << endl;
+    exit(1);
+  }
+  vector<double> q_vec;
+  vector<double> i_exp_vec;
+  double _q, _i_exp;
+  // cout << "cin: q, i" << endl;
+  while (fin >> _q >> _i_exp) {
+  //   cout << "  " << _q << ", " << _i_exp << endl;
+    q_vec.push_back(_q);
+    i_exp_vec.push_back(_i_exp);
+  }
+  fin.close();
+  src = filename;
+
+  this->q_exp = Map<VectorXd>(q_vec.data(), q_vec.size());
+  this->i_exp = Map<VectorXd>(i_exp_vec.data(), i_exp_vec.size());
+  // cout << "Eigen::Vector: q, i" << endl;
+  // for (int i = 0; i < q_exp.size(); i++) {
+  //   cout << "  " << q_exp(i) << ", " << i_exp(i) << endl;
+  // }
+  this->i_par.resize(q_exp.size());
+  Eigen::VectorXd w = q_exp.array() * R_PARTICLE;
+  this->i_par =
+      (3 * (sin(w.array()) - w.array() * cos(w.array())) / (w.array().pow(3)))
+          .square();
+}
+
+void Simulator::set_q_range(const double _q_min, const double _q_max) {
+  assert(this->q_exp.size() > 0 && this->i_exp.size() == this->q_exp.size() &&
+         this->i_par.size() == this->q_exp.size());
+  assert(0 <= _q_min && _q_min < _q_max);
+  int _q_min_idx = -1, _q_max_idx = -1;
+  for (int i = 0; i < q_exp.size(); i++) {
+    if (_q_min_idx == -1 && q_exp(i) >= _q_min) {
+      _q_min_idx = i;
+    }
+    if (q_exp(i) <= _q_max) {
+      _q_max_idx = i;
+    }
+  }
+  assert(_q_min_idx != -1 && _q_max_idx != -1);
+
+  this->q_min = _q_min;
+  this->q_max = _q_max;
+  Eigen::VectorXd _q_exp = this->q_exp;
+  Eigen::VectorXd _i_exp = this->i_exp;
+  Eigen::VectorXd _i_par = this->i_par;
+  SLICE(this->i_exp, _i_exp, _q_min_idx, _q_max_idx);
+  SLICE(this->i_par, _i_par, _q_min_idx, _q_max_idx);
+  SLICE(this->q_exp, _q_exp, _q_min_idx, _q_max_idx);
+  // cout << "stripped: q, i" << endl;
+  // for (int i = 0; i < this->q_exp.size(); i++) {
+  //   cout << "  " << this->q_exp(i) << ", " << this->i_exp(i) << endl;
+  // }
+}
+
+void Simulator::run(const int _max_iter, const double _res_thresh,
+                    const double _sigma2, const int move_per_step) {
+  this->max_iter = _max_iter;
+  this->res_thresh = _res_thresh;
+  this->sigma2 = _sigma2;
+  this->move_per_step = move_per_step;
+
+  residual = compute_residual();
+  residual_hist.resize(max_iter + 1);
+  residual_hist(0) = residual;
+
+  for (int i = 0; i < max_iter; i++) {
+    step_forword(move_per_step);
+    assert(0 <= i + 1 && i + 1 <= max_iter);
+    assert(residual_hist.size() == max_iter + 1);
+    residual_hist(i + 1) = residual;
+    // cout << "iter " << i + 1 << " residual: " << residual << endl;
+    if (residual < res_thresh) {
+      residual_hist = residual_hist.segment(0, i + 1);
+      break;
+    }
+  }
+}
+
+void Simulator::init() {
+  // x, y, existsの初期化
+  x.resize(n);
+  y.resize(n);
+  exists.resize(Lx, Ly);
+  for (int _x = 0; _x < Lx; _x++) {
+    for (int _y = 0; _y < Ly; _y++) {
+      exists(_x, _y) = -1;
+    }
+  }
+
+  bool failed = 0;
+  vector<int> x_shuffled = range(Lx);
+  vector<int> y_shuffled = range(Ly);
+  for (int i = 0; i < n; i++) {
+    random_shuffle(x_shuffled);
+    bool success_x = 0;
+    for (int _x : x_shuffled) {
+      random_shuffle(y_shuffled);
+      bool success_y = 0;
+      for (int _y : y_shuffled) {
+        bool is_clear = 1;
+        for (auto &p : PROHIBITED) {
+          int __x = MOD(_x + p.x, Lx);
+          int __y = MOD(_y + p.y, Ly);
+          if (exists(__x, __y) > -1) {
+            is_clear = 0;
+            break;
+          }
+        }
+        if (!is_clear) continue;  // y
+        x(i) = _x;
+        y(i) = _y;
+        exists(_x, _y) = i;
+        success_y = 1;
+        break;
+      }
+      if (success_y) {
+        success_x = 1;
+        break;
+      } else {
+        continue;  // x
+      }
+    }
+    if (!success_x) {
+      failed = 1;
+      break;
+    } else {
+      continue;  // i
+    }
+  }
+
+  if (failed) {
+    cerr << "Error: failed to initialize particles" << endl;
+    exit(1);
+  }
+
+  // x_re, y_re, a_re, a_im, i_simの初期化
+  x_re.resize(n);
+  y_re.resize(n);
+  for (int i = 0; i < n; i++) {
+    Vd coord = real_coord(x(i), y(i));
+    x_re(i) = coord.x;
+    y_re(i) = coord.y;
+  }
+  compute_i();
+
+  cout << "Initialization done" << endl;
+  residual = compute_residual();
+  cout << "initial residual: " << residual << endl;
+}
+
+bool Simulator::try_move(const int i, const int d) {
+  assert(0 <= i && i < n);
+  assert(0 <= d && d < 6);
+  Vi before(x(i), y(i));
+  Vi after(MOD(x(i) + STEP[d].x, Lx), MOD(y(i) + STEP[d].y, Ly));
+  // cout << exists.transpose() << endl;
+  // cout << "move " << i << " from (" << before.x << ", " << before.y << ") to
+  // ("
+  //      << after.x << ", " << after.y << ")" << endl;
+  assert(exists(before.x, before.y) == i);
+  assert(exists(after.x, after.y) == -1);
+
+  for (auto &p : PROHIBITED) {
+    int __x = MOD(after.x + p.x, Lx);
+    int __y = MOD(after.y + p.y, Ly);
+    if (exists(__x, __y) == i) continue;
+    if (exists(__x, __y) > -1) return 0;
+  }
+
+  x(i) = after.x;
+  y(i) = after.y;
+  exists(before.x, before.y) = -1;
+  exists(after.x, after.y) = i;
+
+  Vd coord = real_coord(x(i), y(i));
+  x_re(i) = coord.x;
+  y_re(i) = coord.y;
+  return 1;
+}
+
+void Simulator::compute_i() {
+  a_re.resize(q_exp.size(), N_THETA);
+  a_im.resize(q_exp.size(), N_THETA);
+  i_sim.resize(q_exp.size());
+  for (int _qi = 0; _qi < q_exp.size(); _qi++) {
+    double _q = q_exp(_qi);
+    for (int _theta = 0; _theta < N_THETA; _theta++) {
+      double _theta_rad = _theta * D_THETA;
+      double _qx = _q * cos(_theta_rad), _qy = _q * sin(_theta_rad);
+      VectorXd re(n), im(n);
+      re = cos(-(_qx * x_re.array() + _qy * y_re.array()));
+      im = sin(-(_qx * x_re.array() + _qy * y_re.array()));
+      a_re(_qi, _theta) = re.sum();
+      a_im(_qi, _theta) = im.sum();
+    }
+  }
+
+  VectorXd _re = a_re.rowwise().sum();
+  VectorXd _im = a_im.rowwise().sum();
+  i_sim = i_par.array() * (_re.array().square() + _im.array().square()) *
+          D_THETA / (2 * M_PI);
+  i_sim *= i_exp.sum() / i_sim.sum();
+}
+
+void Simulator::update_i(const Vd &before, const Vd &after) {
+  // 複素振幅の更新
+  for (int _qi = 0; _qi < q_exp.size(); _qi++) {
+    double _q = q_exp(_qi);
+    for (int _theta = 0; _theta < N_THETA; _theta++) {
+      double _theta_rad = _theta * D_THETA;
+      double _qx = _q * cos(_theta_rad), _qy = _q * sin(_theta_rad);
+      assert(0 <= _qi && _qi < q_exp.size());
+      assert(0 <= _theta && _theta < N_THETA);
+      assert(a_re.rows() == q_exp.size() && a_re.cols() == N_THETA);
+      assert(a_im.rows() == q_exp.size() && a_im.cols() == N_THETA);
+      a_re(_qi, _theta) += -cos(-(_qx * before.x + _qy * before.y)) +
+                           cos(-(_qx * after.x + _qy * after.y));
+      a_im(_qi, _theta) += -sin(-(_qx * before.x + _qy * before.y)) +
+                           sin(-(_qx * after.x + _qy * after.y));
+    }
+  }
+
+  // 散乱強度の更新
+  VectorXd _re = a_re.rowwise().sum();
+  VectorXd _im = a_im.rowwise().sum();
+  assert(_re.size() == q_exp.size() && _im.size() == q_exp.size());
+  i_sim = i_par.array() * (_re.array().square() + _im.array().square()) *
+          D_THETA / (2 * M_PI);
+  i_sim *= i_exp.sum() / i_sim.sum();
+}
+
+double Simulator::compute_residual() const {
+  // 和で正規化されていることを確認
+  assert(abs(i_sim.sum() / i_exp.sum() - 1) < 1e-6);
+  return (i_sim - i_exp).array().square().sum();
+}
+
+void Simulator::step_forword(const int n_move) {
+  // 評価で棄却された場合に戻すためにコピーを取っておく(値渡し)
+  MatrixXd before_a_re = a_re;
+  MatrixXd before_a_im = a_im;
+  VectorXd before_i_sim = i_sim;
+  vector<int> move_i_hist(n_move);
+  vector<int> move_d_hist(n_move);
+
+  for (int _n_move = 0; _n_move < n_move; _n_move++) {
+    vector<int> i_shuffled = range(n);
+    random_shuffle(i_shuffled);
+    bool move_success = 0;
+    for (int i : i_shuffled) {
+      if (move_success) break;
+      vector<int> d_shuffled = range(6);
+      random_shuffle(d_shuffled);
+      for (int d : d_shuffled) {
+        if (move_success) break;
+        bool movable = true;
+        Vd before(x_re(i), y_re(i));
+        move_success = try_move(i, d);
+        if (!move_success) continue;
+        Vd after(x_re(i), y_re(i));
+        update_i(before, after);
+        // 後ろから埋めて前から戻せば元通りになる
+        move_i_hist[n_move - 1 - _n_move] = i;
+        move_d_hist[n_move - 1 - _n_move] = d;
+      }
+    }
+    if (!move_success) {
+      cerr << "Error: failed to move any particles" << endl;
+      exit(1);
+    }
+  }
+
+  double after_residual = compute_residual();
+  bool accept =
+      (after_residual < residual) ||
+      (exp(-(after_residual - residual) / sigma2) > (double)rand() / RAND_MAX);
+  if (accept) {
+    residual = after_residual;
+  } else {
+    for (int i = 0; i < n_move; i++) {
+      bool move_success = try_move(move_i_hist[i], REV(move_d_hist[i]));
+      assert(move_success);
+    }
+    a_re = before_a_re;
+    a_im = before_a_im;
+    i_sim = before_i_sim;
+  }
+}
+
+void Simulator::save_result(const string &filename) const {
+  // クラスタ位置をxyzファイルに保存
+  string xyz = filename + ".xyz";
+  ofstream f_xyz(filename + ".xyz");
+  if (!f_xyz) {
+    cerr << "Error: cannot open file " << xyz << endl;
+    exit(1);
+  }
+  f_xyz << n << endl;
+  f_xyz << "Lx=" << Lx << " Ly=" << Ly << " A_MG=" << A_MG << endl;
+  for (int i = 0; i < n; i++) {
+    f_xyz << "L " << x_re(i) << " " << y_re(i) << " 0" << endl;
+  }
+  f_xyz.close();
+
+  // 格子座標でも保存
+  string lattice = filename + "_lattice.dat";
+  ofstream f_lattice(lattice);
+  if (!f_lattice) {
+    cerr << "Error: cannot open file " << lattice << endl;
+    exit(1);
+  }
+  f_lattice << ">>> Lx:" << Lx << ", Ly:" << Ly << ", N:" << n;
+  f_lattice << " x=[A_MG,0], y=[-A_MG/2, A_MG*sqrt(3)/2], A_MG:" << A_MG;
+  f_lattice << " expfile:" << src << endl;
+  f_lattice << x.transpose() << endl;
+  f_lattice << y.transpose() << endl;
+
+  // 散乱強度をファイルに保存
+  string iq = filename + "_iq.dat";
+  ofstream f_iq(iq);
+  if (!f_iq) {
+    cerr << "Error: cannot open file " << iq << endl;
+    exit(1);
+  }
+  for (int _qi = 0; _qi < q_exp.size(); _qi++) {
+    f_iq << q_exp(_qi) << " " << i_sim(_qi) << endl;
+  }
+  f_iq.close();
+
+  // residualの履歴をファイルに保存
+  string res = filename + "_residual.dat";
+  ofstream f_res(res);
+  if (!f_res) {
+    cerr << "Error: cannot open file " << res << endl;
+    exit(1);
+  }
+  for (int i = 0; i < residual_hist.size(); i++) {
+    f_res << i << " " << residual_hist[i] << endl;
+  }
+
+  // パラメータをファイルに保存
+  string param = filename + "_param.yaml";
+  ofstream f_param(param);
+  if (!f_param) {
+    cerr << "Error: cannot open file " << param << endl;
+    exit(1);
+  }
+  f_param << "Lx: " << Lx << endl;
+  f_param << "Ly: " << Ly << endl;
+  f_param << "n: " << n << endl;
+  f_param << "A_MG: " << A_MG << endl;
+  f_param << "R_PARTICLE: " << R_PARTICLE << endl;
+  f_param << "N_THETA: " << N_THETA << endl;
+  f_param << "max_iter: " << max_iter << endl;
+  f_param << "n_iter_done: " << residual_hist.size() - 1 << endl;
+  f_param << "res_thresh: " << res_thresh << endl;
+  f_param << "sigma2: " << sigma2 << endl;
+  f_param << "move_per_step: " << move_per_step << endl;
+  f_param << "q_min: " << q_min << endl;
+  f_param << "q_max: " << q_max << endl;
+  f_param << "src_file: " << src << endl;
+  f_param << "mt19937_seed: " << seed << endl;
+  f_param << endl;
+  f_param << "q[nm^-1]: [ " << q_exp.transpose() << "]" << endl;
+  f_param << "i_exp: [ " << i_exp.transpose() << "]" << endl;
+
+  f_param.close();
+}
